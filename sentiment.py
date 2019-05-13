74faff0ef60868c6b08dbf22dbe46a150ae08e9b
bcprods <briancoxxie3@gmail.com>
2019-05-13 16:35:33 -0700

    Added sentiment

--- sentiment.py
+++ sentiment.py
@@ -1,2 +1,18 @@
-# analyze_sentiment(content)
+import re
 
+def get_sentiment_dictionary():
+    sentiwords_dict = {}
+    sentiwords = open('config/sentiwords.txt', 'r')
+    for line in sentiwords.readlines():
+        columns = re.split('#[a-z]', line.replace('\n', '').replace('\t', ''))
+        if len(columns) > 1:
+            sentiwords_dict[columns[0]] = float(columns[1])
+    return sentiwords_dict
+
+def get_sentiment_score(string, sentiment_dict):
+    sentiment = 0
+    word_list = re.sub('[^\w]', ' ', string).split()
+    for word in word_list:
+        if word in sentiment_dict:
+            sentiment += sentiment_dict[word]
+    return sentiment
