a34d3d917c25b242ef26b74eb3a01100a3e6080a
Cole <cole.brossart@gmail.com>
2019-06-13 09:49:26 -0700

    Add symbol options to config

--- config.py
+++ config.py
@@ -7,3 +7,22 @@ REDDIT_CONFIG = dict(crypto_subreddits=['CryptoCurrency', 'CryptocurrencyICO', '
 
 FILES = dict(sentiment_dict='files/sentiwords.txt')
 
+# Todo: choose a strategy. Can also put in separate json file
+# Strategy 1
+SYMBOLS = dict(btc=dict(name='Bitcoin', type='cryptocurrency', keywords=['btc', 'bitcoin']),
+               eth=dict(name='Ethereum', type='cryptocurrency', keywords=['eth', 'ethereum']),
+               amzn=dict(name='Amazon', type='business', keywords=['amzn', 'amazon']),
+               goog=dict(name='Alphabet Inc.', type='business', keywords=['goog', 'google', 'alphabet']))
+
+# Strategy 2
+SYMBOLS = dict(btc=['btc', 'bitcoin'],
+               eth=['eth', 'ethereum'],
+               amzn=['amzn', 'amazon'],
+               goog=['goog', 'google', 'alphabet'])
+
+CRYPTO_SYMBOLS = dict(btc=['btc', 'bitcoin'],
+                      eth=['eth', 'ethereum'])
+
+# Strategy 3
+BIZ_SYMBOLS = dict(amzn=['amzn', 'amazon'],
+                   goog=['goog', 'google', 'alphabet'])
