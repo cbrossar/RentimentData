9ad84c61c506b0150a7a0f3240bf83a9ff8930e6
Cole <cole.brossart@gmail.com>
2019-07-18 17:56:11 -0700

    add sentiment plot

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
-from reddit import get_reddit_posts
-from api import get_posts, get_post_ids, write_and_update_posts, get_posts_by_subreddit
-from config import *
+from api import get_posts_by_date_range
 from datetime import datetime
 import logging
-from logs.config import logger
-from plot import plot, plot_by_hour
+from plot import plot_count_by_hour, plot_sentiment_by_hour
 
-logger.info('Setup logger')
 logger = logging.getLogger('Rentiment.' + __name__)
 
-
-logger.info('Get posts from reddit...')
-posts_data = get_reddit_posts(REDDIT_CONFIG['all_subreddits'], 50)
-
-logger.info('Insert posts into mongo...')
-existing_ids = get_post_ids()
-write_and_update_posts(posts_data, existing_ids)
-
-logger.info('Get posts from mongo...')
-posts = get_posts()
-
-start = datetime(2019, 6, 29, 0, 0, 0)
-end = datetime(2019, 7, 3, 0, 0, 0)
+start = datetime(2019, 7, 1, 0, 0, 0)
+end = datetime(2019, 7, 17, 0, 0, 0)
 
 logger.info('Get posts from ' + str(start) + ' to ' + str(end))
+posts = get_posts_by_date_range(start, end)
 
-# posts = get_posts_by_subreddit('CryptoMarkets')
-posts = get_posts()
+plot_count_by_hour('Rentiment Posts', posts)
 
-# plot('Rentiment Bitcoin', 'publish_date', 'text_sentiment', posts)
+# plot_sentiment_by_hour('Rentiment Sentiment', posts)
 
-plot_by_hour('Rentiment Bitcoin', 'publish_date', posts)
--- plot.py
+++ plot.py
@@ -19,18 +19,40 @@ def plot(title, x_label, y_label, posts):
     plt.show()
 
 
-def plot_by_hour(title, y_label, posts):
+def plot_count_by_hour(title, posts):
 
     hour_count = dict()
 
     for post in posts:
-        post_time = post[y_label].replace(hour=0, minute=0, second=0)
+        post_time = post['publish_date'].replace(minute=0, second=0)
         if post_time in hour_count:
             hour_count[post_time] += 1
         else:
             hour_count[post_time] = 1
 
     plt.plot_date(list(hour_count.keys()), list(hour_count.values()))
+    plt.gcf().autofmt_xdate()
+    plt.title(title)
+    plt.xlabel('publish hour')
+    plt.ylabel('count')
+    plt.show()
+
+
+def plot_sentiment_by_hour(title, posts):
+
+    hour_sentiment = dict()
+
+    for post in posts:
+        post_time = post['publish_date'].replace(hour=0, minute=0, second=0)
+        if post_time in hour_sentiment:
+            hour_sentiment[post_time].append(post['title_sentiment'])
+        else:
+            hour_sentiment[post_time] = [post['title_sentiment']]
+
+    for hour in hour_sentiment:
+        hour_sentiment[hour] = sum(hour_sentiment[hour])/len(hour_sentiment[hour])
+
+    plt.plot_date(list(hour_sentiment.keys()), list(hour_sentiment.values()))
     plt.gcf().autofmt_xdate()
     plt.title(title)
     plt.xlabel('publish hour')
